92bbf0668953fc934491082ed2110ae3acaa59f4
ngocPm <irisclothing03@gmail.com>
2024-12-11 21:04:01 +0700

    fix deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,32 @@
 /* eslint-disable no-undef */
 
-const plugin = require("tailwindcss/plugin");
+import plugin from "tailwindcss/plugin";
 
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  corePlugins: {
-    container: false,
-  },
-  theme: {
-    extend: {
-      colors: {
-        primary: "#FBD77F",
-        secondary: "#FCB203",
-        back: "#1B6392",
-      },
+export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"];
+export const corePlugins = {
+  container: false,
+};
+export const theme = {
+  extend: {
+    colors: {
+      primary: "#FBD77F",
+      secondary: "#FCB203",
+      back: "#1B6392",
     },
   },
-  plugins: [
-    plugin(function ({ addComponents, theme }) {
-      addComponents({
-        ".container": {
-          maxWidth: theme("columns.7xl"),
-          marginLeft: "auto",
-          marginRight: "auto",
-          // paddingLeft: theme("spacing.4"),
-          // paddingRight: theme("spacing.4"),
-        },
-      });
-    }),
-  ],
 };
+export const plugins = [
+  plugin(function ({ addComponents, theme }) {
+    addComponents({
+      ".container": {
+        maxWidth: theme("columns.7xl"),
+        marginLeft: "auto",
+        marginRight: "auto",
+        // paddingLeft: theme("spacing.4"),
+        // paddingRight: theme("spacing.4"),
+      },
+    });
+  }),
+];
